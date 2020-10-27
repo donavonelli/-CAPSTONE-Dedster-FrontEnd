7d6e638b4986d8e82caa8ce19854835ee0fda098
donavonelli <donavonelli@yahoo.com>
2020-10-27 18:22:24 -0400

    barebones connect to django

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import Home from './pages/Home'
+import {useState, useEffect} from 'react'
 import './App.css';
+
 // import Routes from './config/Routes';
 
 function App() {
+  const [colleges, setColleges]= useState([])
+
+  useEffect(function(){
+    fetch("/api/colleges")
+    .then((response) => response.json())
+    .then((json) =>{
+      setColleges(json.data)
+    })
+  },[])
+
   return (
     <div className="App">
 
+      {colleges.map((college)=> (
+        <h4 key={college.id}>{college.name}</h4>
+      ))}
       <Home />
       {/* <Routes /> */}
     </div>
