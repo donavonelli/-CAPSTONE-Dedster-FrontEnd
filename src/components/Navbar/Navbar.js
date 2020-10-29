899db7fee4fb07fcda8989ab7995e20fd1ee08b8
donavonelli <donavonelli@yahoo.com>
2020-10-29 12:01:38 -0400

    Navbar altered

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,21 +1,40 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import PropTypes from 'prop-types';
 import './Navbar.css'
 
-const Navbar = function () {
 
-
-    return (
-        <div>
-        <nav className="navbar">
-            <h5><NavLink to='/'>Home</NavLink></h5>
-            <h5><NavLink to='/services'>Services</NavLink></h5>
-            <h5><NavLink to='/profile'>Profile</NavLink></h5>
-            <h5><NavLink to='/about'>About</NavLink></h5>
-            <h5><NavLink to='/contact'>Contact</NavLink></h5>
-        </nav>
-        </div>
+const Navbar = function (props) {
+    const logged_out_nav = (
+        <ul>
+            <li onClick={()=> props.display_form('login')}>Login</li>
+            <li onClick={()=> props.display_form('signup')}>Sign Up</li>
+        </ul>
     )
+
+    const logged_in_nav = (
+        <ul>
+            <li onClick={props.handle_logout}>Logout</li>
+        </ul>
+    )
+    return (<div>{props.logged_in ? logged_in_nav : logged_out_nav}</div>)
 }
+        // return (
+        //     <div>
+        //     <nav className="navbar">
+        //         <h5><NavLink to='/'>Home</NavLink></h5>
+        //         <h5><NavLink to='/services'>Services</NavLink></h5>
+        //         <h5><NavLink to='/profile'>Profile</NavLink></h5>
+        //         <h5><NavLink to='/about'>About</NavLink></h5>
+        //         <h5><NavLink to='/contact'>Contact</NavLink></h5>
+        //     </nav>
+        //     </div>
+        // )
 
 export default Navbar
+
+Navbar.propTypes = {
+    logged_in: PropTypes.bool.isRequired,
+    display_form: PropTypes.func.isRequired,
+    handle_logout: PropTypes.func.isRequired,
+}
