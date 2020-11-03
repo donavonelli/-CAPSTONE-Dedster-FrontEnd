8d6c560dd9c6f4c35d136888ad9afdaa01bb5f3c
donavonelli <donavonelli@yahoo.com>
2020-11-03 16:33:21 -0500

    Added edit button

--- src/pages/Posts/ShowPage.js
+++ src/pages/Posts/ShowPage.js
@@ -1,11 +1,14 @@
 import React from "react";
 import PostCard from '../../components/Posts/PostCard';
-
+import { Link } from 'react-router-dom';
 import usePosts from '../../hooks/Posts/usePosts';
 
 function PostShow(props) {
   const [post] = usePosts(props.match.params.id);
-  return post ? <div><PostCard post={post} /></div>: <h3>Loading...</h3>;
+  const {_id} = post
+  return post ? <div><PostCard post={post} />
+              <Link to={`/posts/${_id}/edit`} style={{color: 'black'}}>Edit</Link>
+              </div>: <h3>Loading...</h3>;
 }
 
 export default PostShow;
